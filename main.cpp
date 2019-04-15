58f62233b54c94c0cec4f2df5c305020eec7b1bb
phabashi <pejmanHabashi@gmail.com>
2019-04-15 01:13:50 -0400

    Implementing the recursive version.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
+#include <vector>
 
-class Problem {
+class GrayCodeGenerator {
     public:
-        int Solution(int n) {
-        
+    std::vector < int > RecursiveSolution (int n) {
+        if(n == 0)
+            return { 0 };
+        if( n == 1)
+            return { 0, 1 };
+        auto a = RecursiveSolution ( n - 1 );
+        auto highestBit = (1 << (n - 1) );
+        std::vector<int> result;
+        int mask =  0 ;
+        for ( int i=0; i < a.size() ; i++ ) {
+            result.push_back( (  mask & highestBit) | a[i] );
+            mask = ~mask;
+            result.push_back( (  mask & highestBit) | a[i] );
         }
+        
+        return result;
+    }
 };
 
 int main( int argc, char* argv[] ) {
     using namespace std;
 
-    Problem p;
-    p.Solution(2);
+    GrayCodeGenerator p;
+    auto g = p.RecursiveSolution ( 4 );
+    for(int i = 0; i < g.size(); i++) {
+        cout << std::dec << g[i] << " => \t" << std::bitset<8>(g[i]) << endl;
+    }
     return 0;
 }
